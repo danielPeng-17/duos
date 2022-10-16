d4f00502b2b344ce8a2b0e09937a2e31d58c3bee
dannykhuu2 <dannykhuu2@gmail.com>
2022-10-16 18:48:21 -0400

    create resusable navigation bar widget, added to main.dart page

--- duos_ui/lib/main.dart
+++ duos_ui/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:duos_ui/widgets/custom_navigation_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Duos',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
+      home: const RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  int currentPage = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Duos"),
+      ),
+      // body: pages[currentPage],
+      bottomNavigationBar: const CustomNavigationBar(),
     );
   }
 }
