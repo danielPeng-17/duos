a24db3036b11744cf648598224603b8cc13fffae
Kingly Yee <kyee@redcanari.com>
2022-10-19 23:00:12 -0400

    prototype profile creation name

--- duos_ui/lib/screens/profile_creation_name.dart
+++ duos_ui/lib/screens/profile_creation_name.dart
@@ -10,10 +10,12 @@ class ProfileCreationName extends StatefulWidget {
 class _ProfileCreationNameState extends State<ProfileCreationName> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: const Center(
-      child: const MyCustomForm(),
-    ));
+    return const Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: MyCustomForm(),
+      ),
+    );
   }
 }
 
@@ -26,14 +28,35 @@ class MyCustomForm extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         const Padding(
-            padding: EdgeInsets.only(top: 20.0),
-            child: SizedBox(
-              width: 250,
-              height: 150,
-            )),
+          padding: EdgeInsets.only(top: 20.0),
+          child: SizedBox(
+            width: 250,
+            height: 100,
+          ),
+        ),
         const Padding(
           padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-          child: Text("What is your name?", style: TextStyle(fontSize: 25)),
+          child: Text.rich(
+            TextSpan(
+              children: <InlineSpan>[
+                WidgetSpan(
+                  child: Icon(
+                    Icons.create_sharp,
+                    color: Colors.black,
+                    size: 25,
+                  ),
+                ),
+                TextSpan(text: ' Profile Creation'),
+              ],
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+          child: Text("What is your name?",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
         ),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
@@ -60,7 +83,8 @@ class MyCustomForm extends StatelessWidget {
         ),
         Padding(
           padding: const EdgeInsets.symmetric(vertical: 16.0),
-          child: Center(child: ElevatedButton(
+          child: Center(
+              child: ElevatedButton(
             onPressed: () {
               // Validate returns true if the form is valid, or false otherwise.
               // if (_formKey.currentState!.validate()) {
@@ -72,8 +96,7 @@ class MyCustomForm extends StatelessWidget {
               // }
             },
             child: const Text('Next'),
-          )
-          ),
+          )),
         ),
       ],
     );
