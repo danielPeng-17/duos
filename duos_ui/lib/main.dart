5b54bc342ce93a084f22d273247b52cdb280d533
dannykhuu2 <dannykhuu2@gmail.com>
2022-10-30 23:51:34 -0400

    add automatic login using authentication state persistence

--- duos_ui/lib/main.dart
+++ duos_ui/lib/main.dart
@@ -1,11 +1,12 @@
-import 'package:duos_ui/screens/join_page.dart';
-import 'package:duos_ui/screens/profile_creation_bio.dart';
-import 'package:duos_ui/screens/profile_creation_game.dart';
-import 'package:duos_ui/screens/profile_creation_name.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:duos_ui/screens/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:duos_ui/screens/join_page.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -21,7 +22,28 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: const LoginPage(),
+      home: const RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatelessWidget {
+  const RootPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            // temporary, replace with HomePage when it is complete
+            return const LoginPage();
+          } else {
+            return const JoinPage();
+          }
+        },
+      ),
     );
   }
 }
