86c51cb81ab0db60bfa0d7b52e9404f77f8c3eef
BrandonKater <brandonkater@gmail.com>
2022-11-03 19:10:16 -0400

    minor touch ups

--- duos_ui/lib/screens/home_page.dart
+++ duos_ui/lib/screens/home_page.dart
@@ -40,84 +40,63 @@ class HomePage extends StatelessWidget {
         ],
       ),
       body: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Stack(
-            children: <Widget>[
-              ListView(
-                children: [
-                  const Text(
-                    'Shang Chi',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
+        padding: const EdgeInsets.all(16),
+        child: Stack(
+          children: <Widget>[
+            ListView(
+              children: [
+                const Text(
+                  'Shang Chi',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
 
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(5.0),
-                    child: Image.network(
-                        'https://wegotthiscovered.com/wp-content/uploads/2021/09/simu-reddit-e1631092903317.jpeg'),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(5.0),
-                    child: Image.network(
-                        'https://imagesvc.meredithcorp.io/v3/mm/image?q=60&c=sc&rect=0%2C54%2C1997%2C1053&poi=face&w=1997&h=999&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2021%2F09%2F07%2FGettyImages-488099013.jpg'),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(5.0),
-                    child: Image.network(
-                        'https://etcanada.com/wp-content/uploads/2021/08/GettyImages-493170697.jpg?quality=80&strip=all'),
-                  ),
-                  // const Text(
-                  //   'Signed In as',
-                  //   style: TextStyle(fontSize: 16),
-                  // ),
-                  // const SizedBox(height: 8),
-                  // Text(
-                  //   user.email!,
-                  //   style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  // ),
-                  const SizedBox(height: 50),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(5.0),
+                  child: Image.network(
+                      'https://wegotthiscovered.com/wp-content/uploads/2021/09/simu-reddit-e1631092903317.jpeg'),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(5.0),
+                  child: Image.network(
+                      'https://imagesvc.meredithcorp.io/v3/mm/image?q=60&c=sc&rect=0%2C54%2C1997%2C1053&poi=face&w=1997&h=999&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2021%2F09%2F07%2FGettyImages-488099013.jpg'),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(5.0),
+                  child: Image.network(
+                      'https://etcanada.com/wp-content/uploads/2021/08/GettyImages-493170697.jpg?quality=80&strip=all'),
+                ),
+                // const Text(
+                //   'Signed In as',
+                //   style: TextStyle(fontSize: 16),
+                // ),
+                // const SizedBox(height: 8),
+                // Text(
+                //   user.email!,
+                //   style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                // ),
+                const SizedBox(height: 50),
 
-                  // ElevatedButton.icon(
-                  //   style: ElevatedButton.styleFrom(
-                  //       minimumSize: const Size.fromHeight(50)),
-                  //   icon: const Icon(Icons.arrow_back, size: 32),
-                  //   label: const Text(
-                  //     'Sign Out',
-                  //     style: TextStyle(fontSize: 24),
-                  //   ),
-                  //   onPressed: () => FirebaseAuth.instance.signOut(),
-                  // ),
-                ],
-              ),
-              Positioned(
-                  top: 500,
-                  right: 20,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all(const CircleBorder()),
-                      padding:
-                          MaterialStateProperty.all(const EdgeInsets.all(22)),
-                      backgroundColor: MaterialStateProperty.all(
-                          Colors.black.withOpacity(0.75)),
-                      // <-- Button color
-                      overlayColor:
-                          MaterialStateProperty.resolveWith<Color?>((states) {
-                        if (states.contains(MaterialState.pressed)) {
-                          return Colors.red;
-                        } // <-- Splash color
-                      }),
-                    ),
-                    child: const Icon(Icons.close),
-                  )),
-              Positioned(
+                // ElevatedButton.icon(
+                //   style: ElevatedButton.styleFrom(
+                //       minimumSize: const Size.fromHeight(50)),
+                //   icon: const Icon(Icons.arrow_back, size: 32),
+                //   label: const Text(
+                //     'Sign Out',
+                //     style: TextStyle(fontSize: 24),
+                //   ),
+                //   onPressed: () => FirebaseAuth.instance.signOut(),
+                // ),
+              ],
+            ),
+            Positioned(
                 top: 500,
-                left: 20,
+                right: 20,
                 child: ElevatedButton(
                   onPressed: () {},
                   style: ButtonStyle(
@@ -130,15 +109,33 @@ class HomePage extends StatelessWidget {
                     overlayColor:
                         MaterialStateProperty.resolveWith<Color?>((states) {
                       if (states.contains(MaterialState.pressed)) {
-                        return Colors.green;
+                        return Colors.red;
                       } // <-- Splash color
                     }),
                   ),
-                  child: const Icon(Icons.videogame_asset_rounded),
+                  child: const Icon(Icons.close),
+                )),
+            Positioned(
+              top: 500,
+              left: 20,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(const CircleBorder()),
+                  padding: MaterialStateProperty.all(const EdgeInsets.all(22)),
+                  backgroundColor:
+                      MaterialStateProperty.all(Colors.black.withOpacity(0.75)),
+                  // <-- Button color
+                  overlayColor:
+                      MaterialStateProperty.resolveWith<Color?>((states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return Colors.green;
+                    } // <-- Splash color
+                  }),
                 ),
-              ],
+                child: const Icon(Icons.videogame_asset_rounded),
+              ),
             ),
-            const SizedBox(height: 50),
             ElevatedButton.icon(
               style: ElevatedButton.styleFrom(
                   minimumSize: const Size.fromHeight(50)),
