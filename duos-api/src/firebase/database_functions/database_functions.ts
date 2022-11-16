7eaac20b7036bc0f68d7e2bd36e6f0e2adfd3471
Diwei Guan <diwei89@hotmail.com>
2022-11-16 10:56:26 -0500

    Update Edit User Endpoint to also use uids, change name of function to make it clear that its an async function

--- duos-api/src/firebase/database_functions/database_functions.ts
+++ duos-api/src/firebase/database_functions/database_functions.ts
@@ -2,7 +2,7 @@ import database from "../config";
 import { collection, addDoc, getDoc, doc, query, where, getDocs, DocumentReference, updateDoc, arrayRemove, arrayUnion } from "firebase/firestore";
 import { User } from "src/models/user"
 
-export const getUserDoc = async (uid) => {
+export const getUserDocAsync = async (uid) => {
     const q = query(collection(database, "user_profiles"), where("uid", "==", uid));
     const documents = await getDocs(q);
     if(documents.size <= 0) throw new Error("No such user");
@@ -21,7 +21,7 @@ export const createNewUserAsync = async (user: User) => {
 }
 
 export const getUserAsync = async (uid: string) => {
-    const user = await getUserDoc(uid);
+    const user = await getUserDocAsync(uid);
     return user.data();
 }
 
@@ -38,20 +38,20 @@ export const getAllUsersInCategory = async (category_ids: DocumentReference[]): 
 }
 
 export const removeALiked = async (uid: string, idToRemove: string) => {
-    const user = await getUserDoc(uid);
+    const user = await getUserDocAsync(uid);
     const docRef = doc(collection(database, "user_profiles"), user.id);
     await updateDoc(docRef, { likes: arrayRemove(idToRemove) });
 }
 
 export const addALiked = async (uid: string, idToAdd: string) => {
-    const user = await getUserDoc(uid);
+    const user = await getUserDocAsync(uid);
     const docRef = doc(collection(database, "user_profiles"), user.id);
     await updateDoc(docRef, { likes: arrayUnion(idToAdd) });
 }
 
 export const addMatch = async (firstUid: string, secondUid: string) => {
-    const user1 = await getUserDoc(firstUid);
-    const user2 = await getUserDoc(secondUid);
+    const user1 = await getUserDocAsync(firstUid);
+    const user2 = await getUserDocAsync(secondUid);
 
     const document = doc(collection(database, "user_profiles"), user1.id);
     const document2 = doc(collection(database, "user_profiles"), user2.id);
@@ -59,8 +59,9 @@ export const addMatch = async (firstUid: string, secondUid: string) => {
     await updateDoc(document, { matched: arrayUnion(secondUid) });
     await updateDoc(document2, { matched: arrayUnion(firstUid) });
 }
-export const updateUserAsync = async (id: string, editedUser: User) => {
-    const docRef = doc(collection(database, "user_profiles"), id);
+export const updateUserAsync = async (uid: string, editedUser: User) => {
+    const user = await getUserDocAsync(uid);
+    const docRef = doc(collection(database, "user_profiles"), user.id);
     const updateDocument = JSON.stringify(editedUser);
     await updateDoc(docRef, JSON.parse(updateDocument));
 }
